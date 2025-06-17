9da945a75493280c90e5cec78a77174ff1d7af7d
gpsalexvasquez <gpsalexvasquez>
2025-06-16 19:56:09 -0500

    style_chatbot

--- practica/settings.py
+++ practica/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Opcional: solo en desarrollo
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
