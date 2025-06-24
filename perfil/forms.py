e9cab327a08dba29f9aed5123b9116ec3a1b1196
gpsalexvasquez <gpsalexvasquez>
2025-06-23 19:44:14 -0500

    estilos

--- perfil/forms.py
+++ perfil/forms.py
@@ -5,3 +5,13 @@ class PerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(PerfilForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            widget = field.widget
+            # No aplicar a checkbox/multiselect por defecto
+            if not isinstance(widget, (forms.CheckboxInput, forms.RadioSelect)):
+                widget.attrs.update({'class': 'form-control'})
+            # Placeholder opcional con nombre del campo
+            widget.attrs.setdefault('placeholder', field.label)
